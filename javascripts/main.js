e8f0e30eddf6be4ab83e81db482d0f55b32efb53
chen38 <spchen38@163.com>
2015-06-30 21:13:12 +0800

    first
    commit

---
+++ javascripts/main.js
@@ -0,0 +1,47 @@
+console.log('This is Chen38.');
+window.onload = function () {
+    var goTop = document.getElementById('gotop');
+	var clientHeight = document.documentElement.clientHeight;
+    var timer = null;
+    var stop = true;
+
+    window.onscroll = function () {
+		var disTop = document.documentElement.scrollTop || document.body.scrollTop;
+		if(disTop >= clientHeight) {
+			goTop.style.display = 'block';
+		} else {
+			goTop.style.display = 'none';
+		}
+        if(!stop){
+            clearInterval(timer);
+        }
+        stop = false;
+    }
+    
+    window.onmousewheel = function () {
+        if(!stop){
+            clearInterval(timer);
+        }
+        stop = false;
+    }
+	
+	goTop.onmouseover = function () {
+		this.innerHTML = "up";
+	}
+	
+	goTop.onmouseout = function () {
+		this.innerHTML = "go";
+	}
+
+    goTop.onclick = function () {
+        timer = setInterval(function () {
+            var disTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var speed = Math.floor(-disTop / 10);
+            document.documentElement.scrollTop = document.body.scrollTop = disTop + speed;
+            stop = true;
+            if (disTop == 0) {
+                clearInterval(timer);
+            }
+        }, 10);
+    }
+}
