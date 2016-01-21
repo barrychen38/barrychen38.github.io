b1c7c7395c7ef9b8f1756807c9e32a5884217fbd
Barry.chen <Barry.chen>
2016-01-21 21:25:12 +0800

    change js

---
+++ ps/js/main.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+	var name = "#myCanvas";
+	var path = "../img/me_bckg.jpg";
+	var btns = get("btns").getElementsByTagName('span'),
+		len = btns.length;
+	Caman(name, path);
+	for (var i = 0; i < len; i++) {
+		btns[i].save = i;
+		btns[i].onclick = function() {
+			for (var j = 0; j < len; j++) {
+				btns[j].className = "";
+			}
+			btns[this.save].className = "selected";
+			var eff = btns[this.save].dataset.effect;
+			switch (eff) {
+				case "Lomo":
+					Lomo(name, path);
+					break;
+				case "Warm":
+					Warm(name, path);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	function Lomo(n, p) {
+		Caman(n, p, function() {
+			this.lomo().render();
+		});
+	}
+
+	function Warm(n, p) {
+		Caman(n, p, function() {
+			this.love().render();
+		});
+	}
+
+	function get(id) {
+		return document.getElementById(id);
+	}
+}
