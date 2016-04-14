8724b4f68a006c3993a038baaabcb051270ff01e
Barry Chen <Barry.Chen@nurun.com>
2016-04-14 14:28:15 +0800

    first try

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 console.log('I am Adobe.');
 
 // preload images
-var images = [];
-function preloadImg() {
-	for (var i = 0; i < arguments.length; i++) {
-		images[i] = new Image();
-		images[i].src = arguments[i];
-	} 
-}
-preloadImg("img/weibo_red.png", "img/wechat_green.png");
+// var images = [];
+// function preloadImg() {
+// 	for (var i = 0; i < arguments.length; i++) {
+// 		images[i] = new Image();
+// 		images[i].src = arguments[i];
+// 	} 
+// }
+// preloadImg("img/weibo_red.png", "img/wechat_green.png");
 
 // show wechat QRCode
-var timer, timer_show;
-$("#wechat").hover(function() {
-	clearTimeout(timer);
-	timer = setTimeout(function() {
-		$(".qrcode").fadeIn(300);
-	}, 200);}, function() {
-	clearTimeout(timer);
-	timer_show = setTimeout(function() {
-		$(".qrcode").fadeOut(300);
-	}, 300);
-});
-$(".qrcode").hover(function() {
-	clearTimeout(timer_show);
-	$(".qrcode").show();
-}, function() {
-	setTimeout(function() {
-		$(".qrcode").fadeOut(300);
-	}, 300);
-});
+// var timer, timer_show;
+// $("#wechat").hover(function() {
+// 	clearTimeout(timer);
+// 	timer = setTimeout(function() {
+// 		$(".qrcode").fadeIn(300);
+// 	}, 200);}, function() {
+// 	clearTimeout(timer);
+// 	timer_show = setTimeout(function() {
+// 		$(".qrcode").fadeOut(300);
+// 	}, 300);
+// });
+// $(".qrcode").hover(function() {
+// 	clearTimeout(timer_show);
+// 	$(".qrcode").show();
+// }, function() {
+// 	setTimeout(function() {
+// 		$(".qrcode").fadeOut(300);
+// 	}, 300);
+// });
