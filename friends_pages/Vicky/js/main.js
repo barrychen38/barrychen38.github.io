7990d64377c4dd14c7d4edc2109f3e119409723c
Chen38 <spchen38@163.com>
2016-04-24 23:32:49 +0800

    change

--- friends_pages/Vicky/js/main.js
+++ friends_pages/Vicky/js/main.js
@@ -7,18 +7,37 @@ function loadEvent(obj) {
 }
 loadEvent({
 	que: [
+		"img/big_heart.png",
 		"img/v1.jpg",
-		"img/v2.jpg"
+		"img/adidas1.png",
+		"img/adidas2.png",
+		"img/adidas3.png"
 	],
 	handProgress: function(e) {
 		var a = parseInt(e.loaded * 100);
 		$('.progress').html(a);
 	},
 	handComplete: function() {
-		// $('.loadingMask').fadeOut(500);
+		$('.loadingMask').fadeOut(500);
+		setTimeout(function() {
+			$('#box1').addClass('boxShake')
+		}, 1000);
 	}
 });
 // load complete
 $(function(){
 	$('#heart').addClass('jump');
+	// touch box 
+	$('#box1')[0].ontouchend = function() {
+		$('.step1').fadeOut(500);
+		setTimeout(function() {
+			$('.step2').fadeIn(500);
+		}, 500);
+	}
+	$('#box2')[0].ontouchend = function() {
+		$('.step2').fadeOut(500);
+		setTimeout(function() {
+			$('.step3').fadeIn(500);
+		}, 500);
+	}
 });
