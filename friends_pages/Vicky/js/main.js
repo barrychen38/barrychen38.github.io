9eeeaefd8ec5b4bf11bfa50f636200e84545b554
Chen38 <spchen38@163.com>
2016-04-24 22:25:12 +0800

    js change

--- friends_pages/Vicky/js/main.js
+++ friends_pages/Vicky/js/main.js
@@ -8,15 +8,14 @@ function loadEvent(obj) {
 loadEvent({
 	que: [
 		"img/v1.jpg",
-		"img/v2.jpg",
-		"img/UptownFunk.mp3"
+		"img/v2.jpg"
 	],
 	handProgress: function(e) {
 		var a = parseInt(e.loaded * 100);
 		$('.progress').html(a);
 	},
 	handComplete: function() {
-		$('.loadingMask').fadeOut(500);
+		// $('.loadingMask').fadeOut(500);
 	}
 });
 // load complete
