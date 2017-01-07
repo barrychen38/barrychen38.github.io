3451fa20a7b581469ce73fae12aa3e8ba5831459
Barry Chen <spchen38@163.com>
2017-01-07 22:21:28 +0800

    rewrite js and hide gotop rocket when screen less than 1024

--- js/main.js
+++ js/main.js
@@ -1,52 +1,42 @@
-console.log('I am Adobe.');
-
-// preload images
-var images = [];
-function preloadImg() {
-	for (var i = 0; i < arguments.length; i++) {
-		images[i] = new Image();
-		images[i].src = arguments[i];
-	} 
-}
-preloadImg("/img/QRcode.png");
-
-// go top
-var $currentWidth = $(window).width();
-if ($currentWidth <= 980) {
-	$(".gotop").hide();
-} else {
-	$(window).scroll(function(event) {
-		if ($(this).scrollTop() >= 60) {
-			$(".gotop").fadeIn(300);
-		} else {
-			$(".gotop").fadeOut(300);
-		}
-	});
-}
-$(".gotop").click(function(event) {
-	event.preventDefault();
-	$("html, body").animate({'scrollTop': 0}, 300);
-});
-
 $(function() {
+	console.info('I am Adobe.');
+	// go top
+	var $gotop = $(".gotop");
+	var $currentWidth = $(window).width();
+	if ($currentWidth <= 980) {
+		$gotop.hide();
+	} else {
+		$(window).scroll(function(event) {
+			if ($(this).scrollTop() >= 60) {
+				$gotop.fadeIn(300);
+			} else {
+				$gotop.fadeOut(300);
+			}
+		});
+	}
+	$gotop.click(function(event) {
+		event.preventDefault();
+		$("html, body").animate({'scrollTop': 0}, 300);
+	});
 	// show wechat QRCode
+	var $qrcode = $(".qrcode");
 	var timer, timer_show;
 	$(".wechat").hover(function() {
 		clearTimeout(timer);
 		timer = setTimeout(function() {
-			$(".qrcode").fadeIn(300);
+			$qrcode.fadeIn(300);
 		}, 200);}, function() {
 		clearTimeout(timer);
 		timer_show = setTimeout(function() {
-			$(".qrcode").fadeOut(300);
+			$qrcode.fadeOut(300);
 		}, 300);
 	});
-	$(".qrcode").hover(function() {
+	$qrcode.hover(function() {
 		clearTimeout(timer_show);
-		$(".qrcode").show();
+		$qrcode.show();
 	}, function() {
 		setTimeout(function() {
-			$(".qrcode").fadeOut(300);
+			$qrcode.fadeOut(300);
 		}, 300);
 	});
 });
